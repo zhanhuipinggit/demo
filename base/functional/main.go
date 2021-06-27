5ace0f2ddbfa77bb2e612185c7d740f89b75b7b9
湛慧平 <18702529695@163.com>
2021-06-27 20:21:03 +0800

    保存相关修改

---
+++ base/functional/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+)
+
+type HandlerFunc func(w http.ResponseWriter,r *http.Request)
+
+func (f HandlerFunc) ServeHTTP (w http.ResponseWriter,r *http.Request)  {
+	f(w,r)
+}
+
+func main()  {
+	hf := HandlerFunc(HelloHandler)
+
+	resp :=httptest.NewRecorder()
+	req := httptest.NewRequest("GET","/",bytes.NewBuffer([]byte("test")))
+	hf.ServeHTTP(resp,req)
+	bts,_ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(bts))
+
+}
+
+func HelloHandler(res http.ResponseWriter,req *http.Request)  {
+	res.Write([]byte("hello world"))
+}
