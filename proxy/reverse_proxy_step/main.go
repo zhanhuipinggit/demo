d8f29ff33eef29a6237398757f1fc907bd60d5c4
湛慧平 <18702529695@163.com>
2021-06-27 22:34:54 +0800

    负载均衡算法相关

---
+++ proxy/reverse_proxy_step/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+var addr = "127.0.0.1:2002"
+
+func main() {
+	//127.0.0.1:2002/xxx
+	//127.0.0.1:2003/base/xxx
+	rs1 := "http://127.0.0.1:2003/base"
+	url1, err1 := url.Parse(rs1)
+	if err1 != nil {
+		log.Println(err1)
+	}
+	proxy := NewSingleHostReverseProxy(url1)
+	log.Println("Starting httpserver at " + addr)
+	log.Fatal(http.ListenAndServe(addr, proxy))
+}
+
+
+func NewSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+	}
+
+	modifyFunc := func(res *http.Response) error {
+		if res.StatusCode !=200 {
+
+			oldPayload,err := ioutil.ReadAll(res.Body)
+			if err !=nil {
+				return err
+			}
+
+			newPayLoad := []byte("hello"+string(oldPayload))
+			res.Body = ioutil.NopCloser(bytes.NewBuffer(newPayLoad))
+			res.ContentLength = int64(len(newPayLoad))
+			res.Header.Set("Content-Length",fmt.Sprint(len(newPayLoad)))
+		}
+		return nil
+	}
+
+
+	return &httputil.ReverseProxy{Director: director,ModifyResponse: modifyFunc}
+}
+
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
